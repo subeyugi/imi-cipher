c9fcd46a136612758b05aff5291d9ced738aaa1c
subeyugi <pentate86@gmail.com>
2024-09-27 05:30:21 +0900

    a

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import renderHtml from "./renderHtml";
-import getDecipherFromSongId from "./search";
+import getDecipherFromSongIds from "./search";
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const { results } = await env.DB.prepare(
 			"SELECT * FROM songs;"
 		).all();
-		getDecipherFromSongId(env, [1, 2, 3]);
+		getDecipherFromSongIds(env, [1, 2, 3]);
 		return new Response(
 			renderHtml(env, results), {
 				headers:{"content-type": "text/html"}
