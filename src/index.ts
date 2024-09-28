5a7945b5bb484e532729b8a2dc5a88766413be13
subeyugi <pentate86@gmail.com>
2024-09-29 00:08:21 +0900

    submit

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,9 @@ import renderHtmlPost from "./postHtml";
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const { pathname } = new URL(request.url);
-		console.log(pathname);
+		if(pathname == "/submit"){
+			return new Response("post finished!!!");
+		}
 
 		return new Response(
 			renderHtmlPost() ,{
